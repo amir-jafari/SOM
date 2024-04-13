9cc01862605608db1bf8e55d62d73582690f8b0a
Ei Tanaka <eieio.0818@gmail.com>
2024-04-13 13:19:27 -0400

    Update iris_interactive.py

--- examples/Tabular/Iris/py/iris_interactive.py
+++ examples/Tabular/Iris/py/iris_interactive.py
@@ -2,13 +2,14 @@ from NNSOM.plots import SOMPlots
 from NNSOM.utils import *
 
 import numpy as np
+from numpy.random import default_rng
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import load_iris
 from sklearn.preprocessing import MinMaxScaler
-from numpy.random import default_rng
-import os
 from sklearn.linear_model import LogisticRegression
+
+import os
 
 # SOM Parameters
 SOM_Row_Num = 4  # The number of row used for the SOM grid.
@@ -54,10 +55,11 @@ cat = count_classes_in_cluster(y, clust)
 # Items for plots
 perc_sentosa = get_perc_cluster(y, 0, clust)
 iris_class_counts_cluster_array = count_classes_in_cluster(y, clust)
+align = np.arange(len(iris_class_counts_cluster_array[0]))
 num_classes = count_classes_in_cluster(y, clust)
 num_sentosa = num_classes[:, 0]
 
-interactive_dict = {
+int_dict = {
         'data': X,
         'target': y,
         'clust': clust,
@@ -68,63 +70,63 @@ interactive_dict = {
     }
 
 # Interactive hit hist
-fig, ax, patches, text = som.hit_hist(X, textFlag=True, mouse_click=True, **interactive_dict)
+fig, ax, patches, text = som.hit_hist(X, mouse_click=True, **int_dict)
 plt.show()
 
 # Interactive neuron dist
-fig, ax, patches = som.neuron_dist_plot(True, **interactive_dict)
+fig, ax, patches = som.neuron_dist_plot(True, **int_dict)
 plt.show()
 
 # Interactive pie plot
-fig, ax, h_axes = som.plt_pie(iris_class_counts_cluster_array, perc_sentosa, mouse_click=True, **interactive_dict)
+fig, ax, h_axes = som.plt_pie(iris_class_counts_cluster_array, perc_sentosa, mouse_click=True, **int_dict)
 plt.show()
 
 # Interactive plt_top
-fig, ax, patches = som.plt_top(True, **interactive_dict)
+fig, ax, patches = som.plt_top(True, **int_dict)
 plt.show()
 
 # Interactive plt_top_num
-fig, ax, patches, text = som.plt_top_num(True, **interactive_dict)
+fig, ax, patches, text = som.plt_top_num(True, **int_dict)
 plt.show()
 
 # Interactive gray plot
-fig, ax, patches, text = som.gray_hist(X, perc_sentosa, True, **interactive_dict)
+fig, ax, patches, text = som.gray_hist(X, perc_sentosa, True, **int_dict)
 plt.show()
 
 # Interactive color plot
-fig, ax, patches, text, cbar = som.color_hist(X, perc_sentosa, True, **interactive_dict)
+fig, ax, patches, text, cbar = som.color_hist(X, perc_sentosa, True, **int_dict)
 plt.show()
 
 # Interactive plt_nc
-fig, ax, patches = som.plt_nc(True, **interactive_dict)
+fig, ax, patches = som.plt_nc(True, **int_dict)
 plt.show()
 
 # interactive simple grid
-fig, ax, patches, cbar = som.simple_grid(perc_sentosa, num_sentosa, True, **interactive_dict)
+fig, ax, patches, cbar = som.simple_grid(perc_sentosa, num_sentosa, True, **int_dict)
 plt.show()
 
 # interactive stem plot
-fig, ax, h_axes = som.plt_stem(align, height, True, **interactive_dict)
+fig, ax, h_axes = som.plt_stem(align, iris_class_counts_cluster_array, True, **int_dict)
 plt.show()
 
 # interactive line plot
-fig, ax, h_axes = som.plt_wgts(True, **interactive_dict)
+fig, ax, h_axes = som.plt_wgts(True, **int_dict)
 plt.show()
 
 # Interactive Histogram
-fig, ax, h_axes = som.plt_histogram(num1, True, **interactive_dict)
+fig, ax, h_axes = som.plt_histogram(num1, True, **int_dict)
 plt.show()
 
 # Interactive Boxplot
-fig, ax, h_axes = som.plt_boxplot(num1, True, **interactive_dict)
+fig, ax, h_axes = som.plt_boxplot(num1, True, **int_dict)
 plt.show()
 
 # Interactive Violin Plot
-fig, ax, h_axes = som.plt_violin_plot(num1, True, **interactive_dict)
+fig, ax, h_axes = som.plt_violin_plot(num1, True, **int_dict)
 plt.show()
 
 # Interactive Scatter plot
-fig, ax, h_axes = som.plt_scatter(num1, num2, True, True, **interactive_dict)
+fig, ax, h_axes = som.plt_scatter(num1, num2, True, True, **int_dict)
 plt.show()
 
 # Train Logistic Regression on Iris
@@ -137,9 +139,9 @@ print('end training')
 ind_missClass = get_ind_misclassified(y, results)
 tp, ind21, fp, ind12 = get_conf_indices(y, results, 0)
 
-if 'clust' in interactive_dict:
-    del interactive_dict['clust']
+if 'clust' in int_dict:
+    del int_dict['clust']
 
 fig, ax, patches, text = som.cmplx_hit_hist(X, clust, perc_sentosa, ind_missClass, ind21, ind12, mouse_click=True,
-                                            **interactive_dict)
+                                            **int_dict)
 plt.show()
